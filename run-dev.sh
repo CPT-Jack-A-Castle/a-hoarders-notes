2f9afa238b7489497d73e6232859948025cd99d6
Mike Keller <mike.k@blu-web.com>
2021-07-11 22:33:43 -0700

    Add some pre-flight to the run-dev.sh script.

--- run-dev.sh
+++ run-dev.sh
@@ -6,4 +6,5 @@ function get_vaults() {
     jq -r .folders[].path < dendron.code-workspace | paste -sd ' '
 }
 
+npm install
 nodemon -e md -w "$(get_vaults)" "$(pwd)"/node_modules/.bin/dendron buildSite --stage dev --serve
