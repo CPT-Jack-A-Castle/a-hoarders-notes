3ed1fcbbf44d55f3d8c612d9c4c3f1499156eafa
Mike Keller <mkeller@betterview.com>
2021-07-12 10:49:24 -0700

    Make run-dev compatible with Mac.

--- run-dev.sh
+++ run-dev.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 function get_vaults() {
-    jq -r .folders[].path < dendron.code-workspace | paste -sd ' '
+    jq -r .folders[].path < dendron.code-workspace | paste -s -d ' ' -
 }
 
+git pull
 npm install
 nodemon -e md -w "$(get_vaults)" "$(pwd)"/node_modules/.bin/dendron buildSite --stage dev --serve
